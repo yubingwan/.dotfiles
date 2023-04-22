06976e397891c46fa60fd2cb2423f5583fd89fde
Yubing Wan <wan.yubing@gmail.com>
2023-04-22 01:02:07 -0400

    update vim setting

--- .config/nvim/lua/core/colorscheme.lua
+++ .config/nvim/lua/core/colorscheme.lua
@@ -2,22 +2,21 @@
 -- in case it isn't installed
 local status, _ = pcall(vim.cmd, "colorscheme gruvbox-material")
 if not status then
-  print("Colorscheme not found!") -- print error if colorscheme not installed
-  return
+	print("Colorscheme not found!") -- print error if colorscheme not installed
+	return
 end
 
--- set up for transparent 
+-- set up for transparent
 require("transparent").setup({
-  enable = true, -- boolean: enable transparent
-  extra_groups = { -- table/string: additional groups that should be cleared
-    -- In particular, when you set it to 'all', that means all available groups
-    -- example of akinsho/nvim-bufferline.lua
-    "BufferLineTabClose",
-    "BufferlineBufferSelected",
-    "BufferLineFill",
-    "BufferLineBackground",
-    "BufferLineSeparator",
-    "BufferLineIndicatorSelected",
-  },
-  exclude = {}, -- table: groups you don't want to clear
+	extra_groups = { -- table/string: additional groups that should be cleared
+		-- In particular, when you set it to 'all', that means all available groups
+		-- example of akinsho/nvim-bufferline.lua
+		"BufferLineTabClose",
+		"BufferlineBufferSelected",
+		"BufferLineFill",
+		"BufferLineBackground",
+		"BufferLineSeparator",
+		"BufferLineIndicatorSelected",
+	},
+	exclude_groups = {}, -- table: groups you don't want to clear
 })

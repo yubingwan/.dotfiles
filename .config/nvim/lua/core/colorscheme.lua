2e92639be402a8b4717e291cab5b538c4c71916a
Yubing Wan <wan.yubing@gmail.com>
2023-08-15 00:03:32 -0400

    update neovim setting

--- .config/nvim/lua/core/colorscheme.lua
+++ .config/nvim/lua/core/colorscheme.lua
@@ -1,14 +1,17 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme gruvbox-material")
+local status, _ = pcall(vim.cmd, [[colorscheme gruvbox-material]])
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
-	return
-end
+ 	return
+ end
+
+-- vim.o.colorscheme = "gruvbox-material"
 
 -- set up for transparent
 require("transparent").setup({
-	extra_groups = { -- table/string: additional groups that should be cleared
+	extra_groups = {
+		-- table/string: additional groups that should be cleared
 		-- In particular, when you set it to 'all', that means all available groups
 		-- example of akinsho/nvim-bufferline.lua
 		"BufferLineTabClose",
--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,27 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+	renderer = {
+		icons = {
+			glyphs = {
+				folder = {
+					arrow_closed = "", -- arrow when folder is closed
+					arrow_open = "", -- arrow when folder is open
+				},
+			},
+		},
+	},
+	-- disable window_picker for
+	-- explorer to work well with
+	-- window splits
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = false,
+			},
+		},
+	},
+})
 
-vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set("n", "<c-n>", ":NvimTreeFindFileToggle<CR>")
